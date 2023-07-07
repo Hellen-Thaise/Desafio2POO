a557e89b53c1e044b94466967b47018668b48a45
Hellen-Thaise <hellen.santos@zup.com.br>
2023-07-07 09:48:25 -0300

    Subindo desafio POO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Main {
+
+    //chamando método para executar o código
     public static void main(String[] args) throws Exception {
         Sistema.executar();
     }
